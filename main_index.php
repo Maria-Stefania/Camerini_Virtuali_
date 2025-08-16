7e306c34f8edfe2c75a91903f02c276b3065d97d
Maria-Stefania <147998122+Maria-Stefania@users.noreply.github.com>
2025-08-16 09:11:31 +0200

    Update main_index.php
    Senza l'header Allow-Headers, il browser blocca le richiestche contengono Authorization header con il JWT token

--- main_index.php
+++ main_index.php
@@ -2,3 +2,5 @@
 session_start();
 require_once 'config/database.php';
 require_once 'config/auth.php';
+header('Access-Control-Allow-Origin: *');         
+header('Access-Control-Allow-Methods: GET, POST'); 
