0644fa8107f52f9b2797a352800d108d9ee8f557
Maria-Stefania <147998122+Maria-Stefania@users.noreply.github.com>
2025-08-20 17:56:34 +0200

    Update login_controller.php
    intestazione standard e inserimento della chiave 'messaggio' diversa dal resto del codice (inconsistenza)

--- login_controller.php
+++ login_controller.php
@@ -1,1 +1,10 @@
+<?php
+header('Content-type: text/json'); 
+require_once '../../config/database.php';
+require_once '../../config/auth.php';
 
+if ($_SERVER['REQUEST_METHOD'] != 'POST') { 
+    http_response_code(405);
+    echo json_encode(['messaggio' => 'Metodo non consentito']); 
+    exit;
+}
