c01678d1158ef90b7bbbce6005d4db298097850a
Maria-Stefania <147998122+Maria-Stefania@users.noreply.github.com>
2025-08-20 18:24:02 +0200

    Update fitting_controller.php
    viene impostata l’intestazione della risposta HTTP per dire al client che i dati inviati sono in formato JSON, Include i file di configurazione del database e dell’autenticazione, e poi richiama una funzione per verificare se l'utente si è già autenticato

--- fitting_controller.php
+++ fitting_controller.php
@@ -1,1 +1,13 @@
+<?php
+header('Content-type: text/json'); 
 
+require_once '../../config/database.php'; 
+require_once '../../config/auth.php';
+
+$user = requireAuth; 
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') { 
+    http_response_code(405);
+    echo json_encode(['messaggio' => 'Metodo non consentito']); 
+    exit;
+}
