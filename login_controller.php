3ad16d381090bd3e6073176b7606cf1883a940b5
Maria-Stefania <147998122+Maria-Stefania@users.noreply.github.com>
2025-08-20 18:12:17 +0200

    Update login_controller.php

--- login_controller.php
+++ login_controller.php
@@ -65,7 +65,7 @@ try {
     ]);
     
 } catch (Exception $e) {
-    error_log('Errore login ' . $e->getMessage());
+    error_log('Errore login: ' . $e->getMessage());
     http_response_code(500);
     echo json_encode(['message' => 'Errore durante il login']);
 }
